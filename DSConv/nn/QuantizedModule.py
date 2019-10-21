c524c6879951c06f3a54c2e63606966b337daa60
marcelo <marcelo@robots.ox.ac.uk>
2019-10-21 14:40:35 +0100

    Added 2bits and 4bits cases in saving and loading state_dict to save memory

--- DSConv/nn/QuantizedModule.py
+++ DSConv/nn/QuantizedModule.py
@@ -14,13 +14,18 @@ class QuantizedModule(torch.nn.Module):
     def quantize(self):
         [m.quantize() for m in self.modules() if isinstance(m, DSConv2d)]
 
-    def state_dict_quant(self, destination = None, prefix='', keep_vars=False):
+    def state_dict_quant(self, destination = None, prefix='', keep_vars=False, block_size=32, bits=8):
         _state_dict_quant_ = {}
         for name, m in self.named_modules():
             if isinstance(m, DSConv2d):
-                print(name)
                 _state_dict_quant_[name + '.alpha'] = m.alpha
-                _state_dict_quant_[name + '.intw'] = m.intw.to(dtype=torch.int8)
+
+                if bits==4:
+                    _state_dict_quant_[name + '.intw'] = self.__compress_to_4_bits__(m)
+                elif bits==2:
+                    _state_dict_quant_[name + '.intw'] = self.__compress_to_2_bits__(m)
+                else: 
+                    _state_dict_quant_[name + '.intw'] = m.intw.to(dtype=torch.int8)
             
             if isinstance(m, torch.nn.Linear):
                 _state_dict_quant_[name+'.bias'] = m.bias
@@ -30,17 +35,23 @@ class QuantizedModule(torch.nn.Module):
                 _state_dict_quant_[name+'.weight'] = m.weight
                 _state_dict_quant_[name+'.bias'] = m.bias
                 _state_dict_quant_[name+'.running_mean'] = m.running_mean
-                _state_dict_quant_[name+'.running_var']  = m.running_var
+                _state_dict_quant_[name+'.running_var'] = m.running_var
                 _state_dict_quant_[name+'.num_batches_tracked'] = m.num_batches_tracked
 
         return _state_dict_quant_
 
-    def load_state_dict_quant(self, state_dict_quant):
+    def load_state_dict_quant(self, state_dict_quant, block_size=32, bits=8):
         for name, m in self.named_modules():
             if isinstance(m, DSConv2d):
                 m.alpha = state_dict_quant[name + '.alpha']
-                m.alpha = m.alpha.repeat_interleave(32, dim = 1)
-                m.intw = state_dict_quant[name + '.intw'].to(dtype=torch.float32)
+                m.alpha = m.alpha.repeat_interleave(block_size, dim=1)
+
+                if bits == 4:
+                    m.intw = self.__decompress_to_4_bits__(state_dict_quant[name + '.intw'], m)
+                elif bits == 2:
+                    m.intw = self.__decompress_to_2_bits__(state_dict_quant[name + '.intw'], m)
+                else:
+                    m.intw = state_dict_quant[name + '.intw'].to(dtype=torch.float32)
                 shp = m.intw.shape
                 m.alpha = m.alpha[:, :shp[1], ...]  # in case shp[1] is not a multiple of 32 
 
@@ -58,3 +69,65 @@ class QuantizedModule(torch.nn.Module):
                 m.running_var = state_dict_quant[name+'.running_var']
                 m.num_batches_tracked = state_dict_quant[name+'.num_batches_tracked']
 
+    def __compress_to_2_bits__(self, module):
+        val = module.intw.flatten()
+        val1 = val[:int(val.size()[0]/4)] + 2
+        val2 = val[int(val.size()[0]/4):int(val.size()[0]/2)] + 2
+        val3 = val[int(val.size()[0]/2):3*int(val.size()[0]/4)] + 2
+        val4 = val[3*int(val.size()[0]/4):] + 2
+
+        val_res = val1 + (2**2)*val2 + (2**4)*val3 + (2**6)*val4
+
+        return val_res.to(dtype=torch.uint8)
+
+    def __decompress_to_2_bits__(self, tensor, module):
+        val_res = tensor
+        val4 = (val_res/(2**6)).to(dtype=torch.int32)
+        val4 = val4.to(dtype=torch.float32)
+        val_res = val_res.to(dtype=torch.float32) - (2**6)*val4
+
+        val3 = (val_res/(2**4)).to(dtype=torch.int32)
+        val3 = val3.to(dtype=torch.float32)
+        val_res = val_res.to(dtype=torch.float32) - (2**4)*val3
+
+        val2 = (val_res/2**2).to(dtype=torch.int32)
+        val2 = val2.to(dtype=torch.float32)
+
+        val1 = val_res.to(dtype=torch.float32) - (2**2)*val2
+        val1.to(dtype=torch.float32)
+
+        val1 = val1 - 2
+        val2 = val2 - 2
+        val3 = val3 - 2
+        val4 = val4 - 2
+
+        val = torch.cat((val1, val2, val3, val4))
+        val = val.unsqueeze(1).unsqueeze(1).unsqueeze(1)
+        val = val.reshape(module.intw.shape)
+
+        return val
+
+    def __compress_to_4_bits__(self, module):
+        val = module.intw.flatten()
+        val1 = val[:int(val.size()[0]/2)]+8
+        val2 = val[int(val.size()[0]/2):]+8
+        
+        val_res = val1 + (2**4)*val2
+
+        return val_res.to(dtype=torch.uint8)
+    
+    def __decompress_to_4_bits__(self, tensor, module):
+        val_res = tensor
+        val2 = (val_res/(2**4)).to(dtype=torch.int32)
+        val2 = val2.to(dtype=torch.float32)
+        val1 = val_res.to(dtype=torch.float32) - (2**4)*val2
+        val1 = val1.to(dtype=torch.float32)
+
+        val1 = val1 - 8
+        val2 = val2 - 8
+
+        val = torch.cat((val1, val2))
+        val = val.unsqueeze(1).unsqueeze(1).unsqueeze(1)
+        val = val.reshape(module.intw.shape)
+
+        return val
