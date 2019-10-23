40944d35e518f69450f53661f05663d5a6c4596f
marcelo <marcelo@robots.ox.ac.uk>
2019-10-23 14:28:11 +0100

    Updated test cases

--- test_case_activation.py
+++ test_case_activation.py
@@ -1,4 +1,4 @@
-from DSConv.nn.bfp_quantization import BFPQuant
+from DSConv.nn.bfp_quantization import BFPActivationLegacy
 from src.bfpactivation import BFPActivation
 import torch
 import numpy as np
@@ -11,8 +11,8 @@ env["CUDA_LAUNCH_BLOCKING"] = "1"
 
 if __name__ == "__main__":
 
-    func_act = BFPQuant.apply
-    theo_activation = BFPActivation(3)
+    func_act = BFPActivationLegacy(4, 7, 32)
+    theo_activation = BFPActivation(4, 7, 32)
 
     number_blocks = 3
     batch_size = 2
@@ -20,18 +20,18 @@ if __name__ == "__main__":
     width = 3
     height = 3
 
-    act = torch.randn((number_blocks, batch_size, block_size, width, height))
+    act = torch.randn((batch_size, block_size*number_blocks, width, height))
 
-    sol = func_act(act, -127, 128, 3, -7, 7)
+    sol = func_act(act)
     sol_theo = theo_activation(act.cuda()).cpu()
 
     x = PrettyTable()
     col_names = ["orig", "cpp", "pytorch", "error"]
-    x.add_column(col_names[0], act[0, 0, :, 0, 0].numpy())
-    x.add_column(col_names[1], sol_theo[0, 0, :, 0, 0].numpy())
-    x.add_column(col_names[2], sol[0, 0, :, 0, 0].numpy())
+    x.add_column(col_names[0], act[0, :, 0, 0].numpy())
+    x.add_column(col_names[1], sol_theo[0, :, 0, 0].numpy())
+    x.add_column(col_names[2], sol[0, :, 0, 0].numpy())
     x.add_column(
-        col_names[3], sol_theo[0, 0, :, 0, 0].numpy() - sol[0, 0, :, 0, 0].numpy()
+        col_names[3], sol_theo[0, :, 0, 0].numpy() - sol[0, :, 0, 0].numpy()
     )
 
     print(x)
